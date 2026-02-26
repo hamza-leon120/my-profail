d99d31a3053439c6a3e015b313a78c6e838b2950
hmz <125275022+hamza-leon120@users.noreply.github.com>
2026-02-27 00:25:45 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,106 @@
+let ber = document.querySelector('.fa-solid.fa-bars')
+let nav = document.querySelector("nav")
+ber.addEventListener("click",function(){
+    if(window.getComputedStyle(nav).height === "0px"){
+        nav.style.cssText = "height: 365px;"
+    }else {
+        nav.style.cssText = "height: 0px;"
+    }
+})
+
+let ul = document.querySelectorAll("ul li")
+ul.forEach(function(ele){
+    ele.addEventListener('click',function(){
+        let section
+        if(this.innerText.includes(" ")){
+            section = document.querySelector(`.${this.innerText.replace(" ","-")}`)
+        }else {
+            section = document.querySelector(`.${this.innerText}`)
+        }
+        window.scrollTo({
+            top: section.offsetTop ,
+            behavior: "smooth"
+        })
+    })
+})
+
+let slide = document.querySelector(".slide")
+let project = document.querySelectorAll(".slide .project")
+let left = document.querySelector(".arrow.left")
+let right = document.querySelector(".arrow.right")
+let num = 0
+right.addEventListener("click",function(){
+    num++
+    project.forEach(function(ele){
+        if(num < project.length) {
+            ele.style.cssText = `transform: translateX(-${parseInt(window.getComputedStyle(slide).width)* num}px);`
+        }else {
+            num = 0
+            ele.style.cssText = "transform: translate(0px)"
+        }
+    })
+})
+left.addEventListener('click',function(){
+    num--
+    project.forEach(function(ele){
+        if(num < 0) {
+            ele.style.cssText = `transform: translateX(-${parseInt(window.getComputedStyle(slide).width) * (project.length - 1)}px);`
+            num = project.length - 1
+        }else {
+            ele.style.cssText = `transform: translateX(-${parseInt(window.getComputedStyle(slide).width)* num}px);`
+        }
+        
+        })
+})
+
+let facebook = document.querySelectorAll('.facebook')
+facebook.forEach(function(ele){
+    ele.addEventListener('click',function (){
+        window.open("https://www.facebook.com/profile.php?id=61584760172770")
+    })
+})
+let github = document.querySelector(".fa-brands.fa-github")
+github.addEventListener("click",function(){
+    window.open("https://github.com/hamza-leon120")
+})
+let email = document.querySelectorAll(".email")
+email.forEach(function(ele){
+    ele.addEventListener("click",function(){
+        // window.open("mailto:haaymen20@gmail.com","_blank")
+        window.location.href = "mailto:haymen20@gmail.com";
+    })
+})
+let instagram = document.querySelectorAll(".instagram")
+instagram.forEach(function(ele){
+    ele.addEventListener("click",function(){
+        window.location.href = "https://www.instagram.com/hmzdeveloper/"
+        // window.open("https://www.instagram.com/hmzdeveloper/")
+    })
+})
+
+let up = document.querySelector(".up")
+let header = document.querySelector("header")
+let headerC = document.querySelector("header .container")
+window.addEventListener("scroll",function(){
+    if(window.getComputedStyle(nav).height !== "0px") {
+        nav.style.cssText = "height: 0px;"
+    }
+    if(this.scrollY >= 1550) {
+        header.style.cssText = "position: sticky; top: 0 ;height: 95px"
+        headerC.style.cssText = "border-bottom: 4px solid #FFFFFF;"
+    }else {
+        header.style.cssText = "position: none;"
+        headerC.style.cssText = "1px solid #43454D;"
+    }
+    if(this.scrollY > 300) {
+        up.style.cssText = "position: fixed;"
+    }else {
+        up.style.cssText = "display: none;"
+    }
+})
+up.addEventListener('click',function(){
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    })
+})
